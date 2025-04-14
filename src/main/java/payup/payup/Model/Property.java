71ce154ef145d3807c27cad0ebdd290fc35a26c3
ngechemoris1 <ngechemoris001@gmail.com>
2025-04-14 12:18:15 +0300

    naming conventions

--- src/main/java/payup/payup/Model/Property.java
+++ src/main/java/payup/payup/Model/Property.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import payup.payup.model.Floor; 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
